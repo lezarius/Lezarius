7c4a10827101e0d3a1e9397ad563f801a465067f
Xena <anissa.dechamps.27800@gmail.com>
2014-02-24 20:59:25 +0100

    Update trouver.php

--- trouver.php
+++ trouver.php
@@ -11,7 +11,7 @@
 	<?php include 'menu.php'; ?>
 <div class="container">
 
-<h1>Voici les différent moyen de me trouver:</h1>
+<h1>Voici les différents moyens de me trouver :</h1>
 
 <div class="row">
 	<p><a href="http://www.twitter.com/remy_mazabraud" class="zocial twitter">@remy_mazabraud</a>
