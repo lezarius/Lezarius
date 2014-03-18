8fe2b8c0857be5701b278b256f0b47406ae59b2b
Lezarius <contact@starry.pw>
2014-03-18 22:48:46 +0100

    Add w3c 2

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="BootStrap/js/bootstrap.min.js"></script>
-<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
 
 
     
