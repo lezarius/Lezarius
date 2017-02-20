ff99ec5329dc0c1427fc67ea572db716a04cd79d
Lezarius <remy.mazabraud@gmail.com>
2017-02-20 12:09:13 +0100

    Fixe dead ling in footer

--- footer.php
+++ footer.php
@@ -4,12 +4,12 @@
 
             <ul class="breadcrumb">
               <li class="pull-right"><a href="#top">Back to top</a></li>
-              <li><a href="http://blog.lezarius.eu">Blog</a></li>
+              <li><a href="http://blog.lezarius.science">Blog</a></li>
               <li><a href="https://twitter.com/lezarius">Twitter</a></li>
               <li><a href="https://github.com/lezarius">GitHub</a></li>
-              <li><a href="http://remy-mazabraud.eu">Pro</a></li>
+              <li><a href="http://lezarius.tech">Pro</a></li>
             </ul>
-            <p>Créé par<a href="http://lezarius.eu">Lezarius</a>. Pour le contacter cliquez <a href="page/contact.php">ici</a>.</p>
+            <p>Créé par<a href="http://lezariutech">Lezarius</a>. Pour le contacter cliquez <a href="page/contact.php">ici</a>.</p>
           </div>
 
       </footer>
