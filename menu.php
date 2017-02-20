e0a24da51bba45b1c8a8d2695fcefba692704b9d
Lezarius <remy.mazabraud@gmail.com>
2017-02-20 11:59:58 +0100

    fixe logo ? if it's work

--- menu.php
+++ menu.php
@@ -9,8 +9,8 @@
  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a href="http://lezarius.science" class="navbar-brand"><img src="Image/logo100.png"></img></a>
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+          <a href="http://lezarius.science" class="navbar-brand"></a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main"> <img src="Image/logo100.png">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -19,13 +19,13 @@
         <div class="navbar-collapse collapse" id="navbar-main">
           <ul class="nav navbar-nav">
             <li>
-              <a href="trouver">Me trouver?</a>
+              <a href="trouver.php">Me trouver?</a>
             </li>
             
              <li>
-              <a href="project">Mes projets</a>
+              <a href="project.php">Mes projets</a>
             </li>
-            <li><a href="friend">Coups de coeur</a></li>
+            <li><a href="friend.php">Coups de coeur</a></li>
             <li>
               <a href="http://blog.lezarius.science">Le Blog</a>
             </li>
