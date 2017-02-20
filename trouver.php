afed44673879b15680e8a1990fc743cb9825f719
Lezarius <remy.mazabraud@gmail.com>
2017-02-20 12:14:27 +0100

    Rebuild

--- trouver.php
+++ trouver.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Me trouver?</title>
+	<title>SocialNetwork</title>
 	<link rel="stylesheet" type="text/css" href="BootStrap/css/bootstrap.css"> 
 	<link rel="stylesheet" type="text/css" href="BootStrap/css/footer.css">
 	<link rel="stylesheet" type="text/css" href="BootStrap/zocial/zocial.css">
@@ -11,11 +11,11 @@
 	<?php include 'menu.php'; ?>
 <div class="container">
 
-<h1>Voici les différents moyens de me trouver :</h1>
+<h1>Voici les différents moyens de mesuivre :</h1>
 
 <div class="row">
 	<p><a href="http://www.twitter.com/lezarius" class="zocial twitter">@Lezarius</a>
-	<a href="https://bitbucket.org/remy_mazabraud" class="zocial bitbucket">Follow on Bitbucket</a>
+	
 	<a href="https://github.com/lezarius" class="zocial github">Follow on Git</a>
 	<a href="http://steamcommunity.com/profiles/76561198081804720/" class="zocial steam">Follow on Steam</a>
 
@@ -23,8 +23,6 @@
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 
-<a class="twitter-timeline" href="https://twitter.com/search?q=%23lezarius" data-widget-id="433621787121692673">Tweets concernant "#lezarius"</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 </div>
 
